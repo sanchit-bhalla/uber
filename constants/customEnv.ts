bd518353122555b6e17bcd6f54349ac94fd2f80a
Sanchit <sanchitbhalla91@gmail.com>
2024-09-03 09:52:37 +0530

    Removed constants

---
+++ constants/customEnv.ts
@@ -0,0 +1,6 @@
+export const EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY =
+  "pk_test_c3VwZXIta2luZ2Zpc2gtMzUuY2xlcmsuYWNjb3VudHMuZGV2JA";
+export const EXPO_PUBLIC_DATABASE_URL =
+  "postgresql://ryde_owner:MPw58kyjXBhK@ep-young-water-a14hqcyd.ap-southeast-1.aws.neon.tech/ryde?sslmode=require";
+export const EXPO_PUBLIC_SERVER_URL = "https://uber.dev/";
+export const EXPO_PUBLIC_GEOAPIFY_API_KEY = "8fb8567baab846fd81c3cc9e739b40d8";
